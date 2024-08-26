1942aee9b0d079d5b913e343283bb8efef97b078
tiffany <tbcasper16@gmail.com>
2024-08-26 15:44:49 -0500

    first commit; setup react rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
 import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
 
 // Render the App component to the root element
+root.render(<App />);
