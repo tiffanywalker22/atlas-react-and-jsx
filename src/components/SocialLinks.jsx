615d1e630090bd595842e63b2b2a255c1380566c
tiffany <tbcasper16@gmail.com>
2024-08-26 16:50:40 -0500

    probably wasn't needed, but went ahead and adjust the spacing between social media icons; everything now looks like image provied

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SocialLinks = () => {
     return (
-        <div classname="social-links">
-            <a href="https://www.linkedin.com/in/tiffanywalker22/" target="_blank" rel="noopener noreferrer">
+        <div classname="social-links" style={{ display: 'flex', alignItems: 'center'}}>
+            <a href="https://www.linkedin.com/in/tiffanywalker22/" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px' }}>
                 <img src="/src/assets/linkedin.svg" alt="LinkedIn" />
             </a>
-            <a href="https://github.com/tiffanywalker22/atlas-react-and-jsx" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/tiffanywalker22/atlas-react-and-jsx" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px' }}>
                 <img src="/src/assets/github.svg" alt="Github" />
             </a>
         </div>
